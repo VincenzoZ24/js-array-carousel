646721445d7fa72eeecc4d15afb19248fee1a821
VincenzoZ24 <zitov287@gmail.com>
2022-10-26 14:44:27 +0200

    ciclo for

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 const arrImg = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
+
+let album = document.querySelector(".container_img");
+let btnTop = document.querySelector(".btn-top");
+let btnbottom = document.querySelector(".btn-bottom");
+
+for (let i = 0; i < arrImg.length; i++) {
+    let eleImg = document.createElement("img");
+    eleImg.src = arrImg[i];
+    album.append(eleImg);
+}
